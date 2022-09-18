352a3f9eaa4b79605562ed6fbf1897306a94be91
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-09-18 14:39:27 -0700

    added proper error handling & tracing

--- src/filter.rs
+++ src/filter.rs
@@ -1,8 +1,20 @@
+use std::{error::Error, fmt::Display};
+
 use hyper::{
-    header::{Entry, HeaderName, AUTHORIZATION, HOST},
+    header::{Entry, HeaderName, HOST},
     http::HeaderValue,
     HeaderMap,
 };
+
+#[derive(Debug)]
+enum FilterErrorKind {
+    NotFound,
+}
+
+#[derive(Debug)]
+pub struct FilterError {
+    _kind: FilterErrorKind,
+}
 
 #[derive(Clone)]
 pub struct Filter<T> {
@@ -10,18 +22,33 @@ pub struct Filter<T> {
 }
 
 impl Filter<HeaderMap> {
-    pub fn authorization(headers: &mut HeaderMap) -> HeaderValue {
-        Filter::header(headers, AUTHORIZATION).unwrap_or_else(|| HeaderValue::from_static(""))
+    pub fn header(headers: &mut HeaderMap, header: HeaderName) -> Result<HeaderValue, FilterError> {
+        let name = header.clone();
+        match headers.entry(header) {
+            Entry::Occupied(entry) => Ok(entry.get().to_owned()),
+            Entry::Vacant(_) => {
+                tracing::error!("'{}' header not found", name);
+                Err(FilterError {
+                    _kind: FilterErrorKind::NotFound,
+                })
+            }
+        }
     }
 
     pub fn host(headers: &mut HeaderMap) -> HeaderValue {
-        Filter::header(headers, HOST).unwrap_or_else(|| HeaderValue::from_static(""))
+        Filter::<HeaderMap>::header(headers, HOST).unwrap_or_else(|_err| {
+            tracing::error!("{} header not found", HOST);
+            HeaderValue::from_static("unknown")
+        })
     }
+}
 
-    pub fn header(headers: &mut HeaderMap, header: HeaderName) -> Option<HeaderValue> {
-        match headers.entry(header) {
-            Entry::Occupied(entry) => Some(entry.get().to_owned()),
-            Entry::Vacant(_) => None,
+impl Display for FilterError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self._kind {
+            FilterErrorKind::NotFound => write!(f, "filter not found"),
         }
     }
 }
+
+impl Error for FilterError {}
