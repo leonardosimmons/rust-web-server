c57829a7465b9a9e9700cb2d0832455963d9677b
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-09-18 18:06:19 -0700

    added  service

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,20 @@
-use rust_web_server::services::{logging::Logging, HelloWorld};
+use rust_web_server::services::{logging::Logging, timeout::Timeout};
 
 use std::{
+    convert::Infallible,
     net::SocketAddr,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
     },
+    time::Duration,
 };
 
-use hyper::server::conn::Http;
+use hyper::{server::conn::Http, service::service_fn, Body, Request, Response};
 use tokio::net::TcpListener;
 use tracing_subscriber::EnvFilter;
 
+const DEFAULT_TIMEOUT: u64 = 5;
 const PROGRAM_TRACING_FILTER: &str = "main=trace";
 const SYSTEM_TRACING_FILTER: &str = "rust_web_server=trace";
 
@@ -38,14 +41,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let process = async move {
             let connection_count = connection_counter.fetch_add(1, Ordering::AcqRel);
 
-            let svc = HelloWorld;
+            let svc = service_fn(hello_world);
+            let svc = Timeout::new(svc, Duration::from_secs(DEFAULT_TIMEOUT));
             let svc = Logging::new(svc, connection_count);
 
             if let Err(err) = Http::new().serve_connection(stream, svc).await {
-                eprintln!("server error: {}", err);
+                tracing::error!("[ request {} ] {}", connection_count, err);
             }
         };
 
         tokio::task::spawn(process);
     }
+}
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    Ok(Response::new("Welcome to the Rust Web Server".into()))
 }
