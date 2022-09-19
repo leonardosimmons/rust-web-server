d1fb129e0d825b19af293b60843f6d5b8ac7cc1c
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-09-18 23:18:50 -0700

    created DEFAULT const vars

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,15 +14,17 @@ use hyper::{server::conn::Http, service::service_fn, Body, Request, Response};
 use tokio::net::TcpListener;
 use tracing_subscriber::EnvFilter;
 
-const DEFAULT_TIMEOUT: u64 = 5;
-const PROGRAM_TRACING_FILTER: &str = "main=trace";
-const SYSTEM_TRACING_FILTER: &str = "rust_web_server=trace";
+const DEFAULT_HOST: [u8; 4] = [127, 0, 0, 1];
+const DEFAULT_PORT: u16 = 5_000;
+const DEFAULT_TIMEOUT: u64 = 30 * 1_000;
+const TRACING_PROGRAM_FILTER: &str = "main=trace";
+const TRACING_SYSTEM_FILTER: &str = "rust_web_server=trace";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let env_filter = EnvFilter::from_default_env()
-        .add_directive(PROGRAM_TRACING_FILTER.parse().unwrap_or_default())
-        .add_directive(SYSTEM_TRACING_FILTER.parse().unwrap_or_default());
+        .add_directive(TRACING_PROGRAM_FILTER.parse().unwrap_or_default())
+        .add_directive(TRACING_SYSTEM_FILTER.parse().unwrap_or_default());
 
     tracing_subscriber::fmt()
         .with_env_filter(env_filter)
@@ -31,7 +33,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let connection_counter = Arc::new(AtomicUsize::new(1));
 
-    let addr: SocketAddr = ([127, 0, 0, 1], 5000).into();
+    let addr: SocketAddr = (DEFAULT_HOST, DEFAULT_PORT).into();
     let listener = TcpListener::bind(addr).await?;
 
     loop {
@@ -41,8 +43,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let process = async move {
             let connection_count = connection_counter.fetch_add(1, Ordering::AcqRel);
 
-            let svc = service_fn(hello_world);
-            let svc = Timeout::new(svc, Duration::from_secs(DEFAULT_TIMEOUT));
+            let svc = service_fn(router);
+            let svc = Timeout::new(svc, Duration::from_millis(DEFAULT_TIMEOUT));
             let svc = Logging::new(svc, connection_count);
 
             if let Err(err) = Http::new().serve_connection(stream, svc).await {
@@ -53,7 +55,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         tokio::task::spawn(process);
     }
 }
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    tokio::time::sleep(Duration::from_secs(1)).await;
+
+async fn router(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Welcome to the Rust Web Server".into()))
 }
